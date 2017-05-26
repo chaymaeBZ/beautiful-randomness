f49e75424aa92bda9af7a234342b7210f133033f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-26 21:40:48 +0000

    respond to html and json format

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,9 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @employees }
+    end
   end
 end
