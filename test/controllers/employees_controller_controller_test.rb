d26a93f89f449182fb2673beb4b9519dbfece13a
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-26 21:32:22 +0000

    modified README

--- test/controllers/employees_controller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EmployeesControllerControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get employees_controller_index_url
-    assert_response :success
-  end
-
-end
